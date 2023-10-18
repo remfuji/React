118a93cc60f0c689a0db5ff0e3e4c92dc33be51c
remfuji <richi.work@gmail.com>
2023-10-18 14:19:48 +0200

    sd

--- simprova/src/App.js
+++ simprova/src/App.js
@@ -2,12 +2,13 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 import Forum from './pages/Forum';
+import Spotify from './pages/spotify/Spotify';
 
 
 function App() {
  
   return (
-      <Forum />
+      <Spotify/>
   );
 }
 
